f296615f19463f634ee3d1e4cd7ba4987a4741b0
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-06-09 02:35:36 +0200

    C#_studies

--- Program.cs
+++ Program.cs
@@ -7,14 +7,40 @@ namespace BA_C_SHARP_8__For_Loop_
         static void Main(string[] args)
         {
             // For Loop
-            // Örnek -- Ekarana 10 kere Hallo yazmak
+            // Örnek - 1:  Ekarana 10 kere Hallo yazmak
             // Baslangic - bitis - artis miktari (Döngünü artis miktari)
-
 
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Hallo_" + i);
             }
+
+            // Örnek - 2: 1'den 100'e kadar cift sayilari bulup ekrana yazdiran algoritmayi tasarlayin.
+
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            // 1 ile 1250 arasinda 7'ye tam bölünebilen X kadar sayi vardir.
+
+            int basket = 0;
+
+            for (int i = 1; i < 1250; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine(i);
+                    basket = basket + 1;
+                }
+            }
+
+            Console.WriteLine("********************");
+            Console.WriteLine($"1 ile 1250 arasinda 7'ye tam bölünebilen {basket}  tane sayi vardir.");
+
         }
     }
 }
