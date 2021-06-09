1037cd1d9842a575adb1aa2a0cc4d895ea7e46dd
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-06-09 02:08:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BA_C_SHARP_8__For_Loop_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // For Loop
+            // Örnek -- Ekarana 10 kere Hallo yazmak
+            // Baslangic - bitis - artis miktari (Döngünü artis miktari)
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hallo_" + i);
+            }
+        }
+    }
+}
